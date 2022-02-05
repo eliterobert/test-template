567a343574280f0b6fafe9df6c800d6cb3a9f7cf
eliterobert <roberteriksson_@hotmail.com>
2022-02-05 10:13:02 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { API } from 'homebridge';
 import { PLATFORM_NAME } from './settings';
 import { ExampleHomebridgePlatform } from './platform';
 
+
 /**
  * This method registers the platform with Homebridge
  */
